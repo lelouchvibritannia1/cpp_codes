e6a6d2c317816cd41fc95e5edf9ea570b4dfa339
schconj <singbsiddhartha@gmail.com>
2023-06-30 03:33:18 +0000

    some changes

--- cp/tempCodeRunnerFile.cpp
+++ cp/tempCodeRunnerFile.cpp
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 cout<<"YES"<<endl;
+=======
+for(int i=1;i<n+1;i++){
+    //     for(int j=0;j<x+1;j++){
+    //         if(j >= pr[i-1]){
+    //             dp[i][j]=max(dp[i-1][j-pr[i-1]]+pg[i-1],dp[i-1][j]);
+    //         }
+    //         else{
+    //             dp[i][j]=dp[i-1][j];
+    //         }
+    //     }
+    // }
+>>>>>>> 09aa612 (some changes)
